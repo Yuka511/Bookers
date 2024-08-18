1b541268d3a07bc168583f63be8f9e1df2286b5c
Yuka511 <174612970+Yuka511@users.noreply.github.com>
2024-08-18 14:23:55 +0000

    [Middle2]3-bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   root to: 'homes#top'
+  resources :books
+  get 'books/:id', to: 'books#show', as: 'show_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
